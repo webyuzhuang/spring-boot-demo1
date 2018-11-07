e70aaca384f7d404941a8a116e3a42aced798343
webyuzhuang <15937177279@163.com>
2018-11-07 16:28:11 +0800

    简单推送

--- spring-boot-demo1/src/main/java/com/daicy/demo1/SrpingBootDemo1Application.java
+++ spring-boot-demo1/src/main/java/com/daicy/demo1/SrpingBootDemo1Application.java
@@ -8,8 +8,6 @@ public class SrpingBootDemo1Application {
 	/*@SpringBootApplication组合注解，兼备了@EnableAutoConfiguration和@ComponentScan 注解的功能。*/
 	//试一下功能好使不
 	public static void main(String[] args) {
-		
-		
 		System.out.println("--------------");
 		
 		SpringApplication.run(SrpingBootDemo1Application.class, args);
